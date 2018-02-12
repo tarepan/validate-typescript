f4ac33b12e00b44f9e61865b76f2ff1383919bc3
Grantosaurus <zietsmangrant@gmail.com>
2018-02-12 10:21:54 +0200

    Restructured

--- dist/checks.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isIso8601(value) {
-    let utcMs = Date.parse(value);
-    return (!Number.isNaN(utcMs) && (value === (new Date(utcMs)).toISOString()));
-}
-exports.isIso8601 = isIso8601;
-//# sourceMappingURL=checks.js.map
--- dist/symbols.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Validator = Symbol("validator");
-exports.Metadata = Symbol("metadata");
-exports.TypeValidator = Symbol("type-validator");
-exports.OptionsValidator = Symbol("options-validator");
-exports.CustomValidator = Symbol("custom-validator");
-//# sourceMappingURL=symbols.js.map
